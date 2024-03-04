22b05c666c73954e68b4edc357dea14602a782f0
td62 <tudou666666a@outlook.com>
2024-03-04 22:37:15 +0800

    order status scheduled

--- SkyTakeout-server/src/main/java/td/mapper/OrderMapper.java
+++ SkyTakeout-server/src/main/java/td/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.annotations.Select;
 import td.dto.OrdersPageQueryDTO;
 import td.entity.Orders;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     /**
@@ -47,4 +50,13 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+    /**
+     * 根据订单状态和时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
---
+++ SkyTakeout-server/src/main/java/td/task/OrderTask.java
@@ -0,0 +1,54 @@
+package td.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import td.entity.Orders;
+import td.mapper.OrderMapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 定时任务,定时处理订单状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    //处理超时订单
+    @Scheduled(cron = "0 * * * * ?")//每分钟触发一次
+    public void processTimeOutOrder() {
+        log.info("处理超时订单:{}", LocalDateTime.now());
+        LocalDateTime time = LocalDateTime.now().minusMinutes(15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            for (Orders orders : ordersList){
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时,自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+
+    }
+    @Scheduled(cron =" 0/10 * * * * ?  " )//10分钟触发一次
+    public void processDeliveryOrder(){
+        log.info("处理派送中订单:{}", LocalDateTime.now());
+        LocalDateTime time = LocalDateTime.now().minusMinutes(60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            for (Orders orders : ordersList){
+                orders.setStatus(Orders.COMPLETED);
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+
+    }
+}
